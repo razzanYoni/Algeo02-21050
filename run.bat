d3293e6f55caeb312033c1a4a1275f525f04e190
razzanYoni <16521378@mahasiswa.itb.ac.id>
2022-11-05 03:54:13 +0700

    -

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @REM buat nanti belum tau yang mana bakal jadi appsnya
 @REM pastiin udah jadi venvnya
+pip install virtualenv
 py -m venv src
 CALL .\src\Scripts\activate
 pip install -r requirements.txt
