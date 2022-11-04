2d77b833628c8c307a22d0f03f7d1d6f992eefa7
razzanYoni <16521378@mahasiswa.itb.ac.id>
2022-11-05 03:16:56 +0700

    hxhxhh

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 @REM buat nanti belum tau yang mana bakal jadi appsnya
+@REM pastiin udah jadi venvnya
+py -m venv src
 CALL .\src\Scripts\activate
+pip install -r requirements.txt
 pyinstaller --onefile .\src\program\apps.py -w
 .\dist\apps.exe
